64753cf177a6a994d9aa3c69b4d6809ba0f66d67
manishz90a <manishz90a@gmail.com>
2022-05-04 14:25:13 +0530

    added index.js and handler for aws lambda

--- server.js
+++ server.js
@@ -2,25 +2,34 @@ const express = require('express');
 const getWeatherInfo = require('./get-weather-info.js');
 const port = process.env.PORT || '3001';
 
-var app = express();
+function createAndRunServer() {
+  var app = express();
 
-app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/public'));
+  
+  app.get('/', (req, res) => {
+    res.sendFile('public/index.html');
+  });
+  
+  app.get('/get_weather_info', (req, res) => {
+      if (req.query.city) {
+          getWeatherInfo.getWeatherInfo(req.query.city).
+          then((data)=>{
+              res.send(data);
+          }, (err) => {
+              res.send(err);
+          });
+      }
+  });
+  
+  app.listen(port, () => {
+    console.log('Server is up on port ' + port);
+  });
+}
 
-app.get('/', (req, res) => {
-  res.sendFile('public/index.html');
-});
+module.exports = {
+  createAndRunServer: createAndRunServer
+}
 
-app.get('/get_weather_info', (req, res) => {
-    if (req.query.city) {
-        getWeatherInfo.getWeatherInfo(req.query.city).
-        then((data)=>{
-            res.send(data);
-        }, (err) => {
-            res.send(err);
-        });
-    }
-});
 
-app.listen(port, () => {
-  console.log('Server is up on port ' + port);
-});
+
